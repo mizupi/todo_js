5d963496e903cdb77c01f780a262941b150a1508
mizupi <98501087+mizupi@users.noreply.github.com>
2022-02-24 12:42:08 +0900

    削除機能の追加

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import "./styles.css";
 
 const onclickAdd = () => {
-  alert();
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  const completedButton = document.createElement("button");
+  completedButton.innerText = "完了";
+  completedButton.addEventListener("click", () => {});
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incompleted-list").removeChild(deleteTarget);
+  });
+
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completedButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("incompleted-list").appendChild(li);
 };
 
 document

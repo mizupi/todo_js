05cba6f299e8bfa16197c3b443b83024b622fe85
mizupi <98501087+mizupi@users.noreply.github.com>
2022-02-24 15:16:19 +0900

    完了機能の追加

--- src/index.js
+++ src/index.js
@@ -5,30 +5,49 @@ const onclickAdd = () => {
   document.getElementById("add-text").value = "";
 
   const li = document.createElement("li");
-
-  const div = document.createElement("div");
-  div.className = "list-row";
+  li.className = "list-row";
 
   const p = document.createElement("p");
   p.innerText = inputText;
 
   const completedButton = document.createElement("button");
   completedButton.innerText = "完了";
-  completedButton.addEventListener("click", () => {});
+  completedButton.addEventListener("click", () => {
+    deleteFromIncompletedList(completedButton.closest("li"));
+
+    const addTarget = completedButton.closest("li");
+
+    const text = addTarget.querySelector("p").innerText;
+
+    addTarget.textContent = null;
+
+    const p = document.createElement("p");
+    p.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("completed-list").appendChild(addTarget);
+  });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.closest("li");
-    document.getElementById("incompleted-list").removeChild(deleteTarget);
+    deleteFromIncompletedList(deleteButton.closest("li"));
   });
 
-  li.appendChild(div);
-  div.appendChild(p);
-  div.appendChild(completedButton);
-  div.appendChild(deleteButton);
+  li.appendChild(p);
+  li.appendChild(completedButton);
+  li.appendChild(deleteButton);
 
   document.getElementById("incompleted-list").appendChild(li);
+};
+
+const deleteFromIncompletedList = (target) => {
+  document.getElementById("incompleted-list").removeChild(target);
 };
 
 document
